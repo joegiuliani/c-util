7e8f1226b615caf598908eb2523d09047b2dfdb6
joeg21 <jggiuliani@gmail.com>
2024-03-31 16:38:07 -0400

    Made str cstr-compatible. Added str_add(str*, str) function.

--- include/cutil/str.h
+++ include/cutil/str.h
@@ -1,9 +1,6 @@
 #ifndef CUTIL_STR_H
 #define CUTIL_STR_H
 
-// -- Todo --
-// Make compatible with c-style strings and add a str_cstr() function.
-
 // -- Includes --
 #include <cutil/vec.h>
 #include <stdint.h>
@@ -26,7 +23,16 @@ void str_init(str* s);
 // or STR_NPOS if none was found
 size_t str_find(str s, char c, size_t offset);
 
-// Creates a new string of s's elements a to b inclusive and puts it in out.
+// Initializes the specified substring from a to min(b-1,s.size-1) and places
+// it in out
 result str_sub(str s, size_t a, size_t b, str* out);
+
+size_t str_len(str s);
+
+// The lifetime of the returned string is managed by the owner of of the passed
+// str
+const char* str_cstr(str s);
+
+result str_adds(str* s, str x);
 
 #endif // CUTIL_STR_H
--- src/cutil/str.c
+++ src/cutil/str.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+void add_null_termination(str* s)
+{
+	((char*)vec_array(*s))[str_len(*s)] = '\0';
+}
+
 void str_init(str* s)
 {
 	vec_init(s, sizeof(char));
@@ -9,17 +14,40 @@ void str_init(str* s)
 
 size_t str_find(str s, char c, size_t offset)
 {
-	if (offset >= s.size) exit(1);
+	if (offset >= s.size - 1) return STR_NPOS;
 	return strchr((char*)s.arr + offset, c);
 }
 
 result str_sub(str s, size_t a, size_t b, str* out)
 {
-	if (a > b || b > s.size - 1) exit(1);
-	const size_t size = b - a + 1;
+	if (a > b) return FAILURE;
+	const size_t sub_size = min(b, str_len(s)) - a;
 	str_init(out);
-	if (vec_set_capacity(out, size) != SUCCESS)
+	if (vec_set_size(out, sub_size+1) != SUCCESS)
 		return FAILURE;
-	if (memcpy(out->arr, (char*)s.arr + a, size) == NULL)
+	add_null_termination(out);
+	if (memcpy(str_cstr(*out), str_cstr(s) + a, sub_size) == NULL)
 		return FAILURE;
+	return SUCCESS;
+}
+
+size_t str_len(str s)
+{
+	return vec_size(s) - 1;
+}
+
+const char* str_cstr(str s)
+{
+	return s.arr;
+}
+
+result str_adds(str* s, str x)
+{
+	const size_t copy_offset = str_len(*s);
+	if (vec_set_size(s, str_len(*s) + str_len(x) + 1) != SUCCESS)
+		return FAILURE;
+	add_null_termination(s);
+	if (memcpy(str_cstr(*s) + copy_offset, str_cstr(x), str_len(x)) == NULL)
+		return FAILURE;
+	return SUCCESS;
 }
